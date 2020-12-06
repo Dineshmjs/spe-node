5fd6bf83dda4f61215569699d3f46a937a92e06a
Dinesh <dineshmj82@gmail.com>
2020-12-06 11:23:13 +0530

    code in goods and sales

--- router/Goods.js
+++ router/Goods.js
@@ -6,4 +6,76 @@ goods.get("/",async(req,res)=>{
     res.send(data) 
 })
 
+// const product = require('express').Router()
+// const goodsSchema = require('../schema/Product') 
+
+
+
+goods.post("/",async(req,res)=>{
+    console.log(req.body)
+    
+
+    const insert = await new goodsSchema({
+        qt:0,
+        ...req.body
+    })
+
+    await insert.save((err,doc)=>{
+        if(doc){
+            // console.log(doc)
+            res.status(200).send(doc)
+        }
+        if(err){
+            console.log(err)
+            res.status(404).send(err)
+        }
+    })
+   
+})
+
+
+goods.get("/autofill",async(req,res)=>{
+    const data =await goodsSchema.findOne({_id:req.query.id})
+    res.json(data)
+})
+
+goods.put("/",async(req,res)=>{
+    const {_id,qt,__v,...data} = req.body
+
+    const find = {
+        _id:_id
+    }
+    const update = {
+        $set:data
+    }
+
+    // console.log(data)
+
+    const updateResponse = await goodsSchema.updateOne(find,update)
+
+    if(updateResponse){
+        // console.log(updateResponse)
+        res.status(200).send(updateResponse)
+    }
+   
+})
+
+goods.delete("/",async(req,res)=>{
+    const deleteData = await goodsSchema.deleteOne({_id:req.query.id})
+    if(deleteData){
+        res.status(200).send(deleteData)
+    }
+})
+
+
+
+
+
+goods.get("/searchProduct",async(req,res)=>{    
+    const data =await goodsSchema.findOne({product:{$regex : req.query.product}})  
+    // console.log(data)  
+    res.send(data)
+})
+
+
 module.exports = goods
